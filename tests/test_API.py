e347b63feef9528fcffa97213db5dbe65edf4863
Xavier Convert <xavier.convert@gmail.com>
2024-01-28 12:43:11 +0100

    maj test_API pour test CI

--- tests/test_API.py
+++ tests/test_API.py
@@ -3,15 +3,19 @@ from ..api import show_data #..src.api
 from ..api import get_client_detail
 from ..api import get_predictions
 #import pandas as pd
-import logging
+#import logging
 
 logging.basicConfig(filename='test.log', level=logging.DEBUG) #, format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S' )
 
 def test_get_ids():
     ids = get_ids()
     print(f'le dataset contient {len(ids.values())} ids')
-    logging.debug(msg='1er essai log')
-    assert len(ids.values()) >= 1
+    #logging.debug(msg='1er essai log')
+    # test qui doit passer
+    #assert len(ids.values()) >= 1
+    
+    # test qui ne doit être KO (test CI/GitHub Actions):
+    assert len(ids.values()) ==0
     
 def test_client_details(cid=101077):
     data = get_client_detail(cid)

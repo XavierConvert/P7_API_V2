f45688f9cc82f09ae940d7371764aea632669b64
Xavier Convert <xavier.convert@gmail.com>
2024-02-18 19:05:15 +0100

    update uvicorn.run for deployment

--- api_V2.py
+++ api_V2.py
@@ -105,6 +105,6 @@ def shap_value(cid:int):
 # 5. Run the API with uvicorn
 #    Will run on http://127.0.0.1:8000
 if __name__ == '__main__':
-    uvicorn.run(api, host='127.0.0.1', port=8000)
-    
+    #uvicorn.run(api, host='127.0.0.1', port=8000)
+    uvicorn.run(app, host='127.0.0.1', port=8000)
     

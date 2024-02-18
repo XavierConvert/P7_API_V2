686c67f2acfde842191ff87d236f274f47c09a23
Xavier Convert <xavier.convert@gmail.com>
2024-02-18 17:53:58 +0100

    update test_API (V2 impacts)

--- tests/test_API.py
+++ tests/test_API.py
@@ -1,7 +1,7 @@
-from ..api import get_ids #..src.api
-from ..api import show_data #..src.api
-from ..api import get_client_detail
-from ..api import get_predictions
+from ..api_V2 import get_ids #..src.api
+from ..api_V2 import show_data #..src.api
+from ..api_V2 import get_client_detail
+from ..api_V2 import get_predictions
 #import pandas as pd
 #import logging
 
@@ -17,23 +17,27 @@ def test_get_ids():
     # test qui ne doit être KO (test CI/GitHub Actions):
     #assert len(ids.values()) ==1
     
-def test_client_details(cid=101077):
+def test_client_details(cid=101400):
     data = get_client_detail(cid)
     print(f'id test = {cid}')
     print(data['AMT_ANNUITY'])
-    assert data['AMT_ANNUITY']==46143
+    assert data['AMT_ANNUITY']==370629
 
 def test_show_data():
     df=show_data()
     print(df.columns.to_list())
-    assert df.columns.to_list()==['SK_ID_CURR','FLAG_OWN_CAR','FLAG_OWN_REALTY','AMT_INCOME_TOTAL','AMT_CREDIT','AMT_ANNUITY','AMT_GOODS_PRICE','CNT_FAM_MEMBERS','EXT_SOURCE_1','EXT_SOURCE_2',
-                     'EXT_SOURCE_3','client_age','client_prof_exp','Cash_loans','GENDER_FEMALE','active_client','relationship']
+    assert df.columns.to_list()==['SK_ID_CURR','FLAG_OWN_CAR','FLAG_OWN_REALTY','AMT_INCOME_TOTAL','AMT_CREDIT','AMT_ANNUITY','AMT_GOODS_PRICE','CNT_FAM_MEMBERS','EXT_SOURCE_1',
+                                  'EXT_SOURCE_2','EXT_SOURCE_3','Prev_contract_nb','Prev_AMT_CREDIT','Refused_rate','default_payment','INCOME_CREDIT_PERC','ANNUITY_INCOME_PERC',
+                                  'PAYMENT_RATE','client_age','INCOME_PER_PERSON','client_prof_exp','Cash_loans','GENDER_FEMALE','active_client','relationship']
 
-def test_get_prediction(cid=101077):
+										
+
+
+def test_get_prediction(cid=101400):
     pred=get_predictions(cid)
     #print (type(pred))
     print (pred["prediction"])
     print (pred['proba_rembour'])
     assert pred["prediction"]=='Crédit accepté'
-    assert pred['proba_rembour']==0.49
+    assert pred['proba_rembour']==0.61
     

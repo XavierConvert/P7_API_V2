e27f95066e13ffe97edc0391f3c517bd5e767f0b
Xavier Convert <xavier.convert@gmail.com>
2024-02-20 18:06:21 +0100

    update API_V2_pref enhancement

--- api_V2.py
+++ api_V2.py
@@ -1,3 +1,25 @@
+#####################################
+########### Script pour API #########
+#####################################
+
+### 1. Import des librairies requises
+
+### 2. Création de l'objet API
+
+### 3. Chargement des fichiers .pkl requis + préparation du fichier shap_values:
+## - Modèle ML
+## - Data
+## - Décomposition du modèle pour le calcul des shap values (imputer / transformer / estimator)
+
+### 4. Défintion des différentes routes / fonctions:
+## 1. message d'accueil
+## 2. list des ids (12299 dans la dernière version chargée)
+## 3. Informations clients => par appel du client id
+## 4. Synthèse du dataset => describe()
+## 5. Prédiction du modèle par appel d'un client (seuil prodict proba = 0.7)
+## 6. Appel shap values => par appel du client id
+
+
 # 1. Import libraries 
 import uvicorn
 from fastapi import FastAPI #, UploadFile
@@ -18,7 +40,7 @@ api = FastAPI()
 
 # 3. Imports of required pkl files:
 
-pkl_1= open("src/best_lgbm.pkl","rb")
+pkl_1= open("src/best_lgbm2.pkl","rb")
 classifier=pd.read_pickle(pkl_1)
 
 pkl_2= open("src/data2pkl_V2.pkl","rb")
@@ -34,7 +56,24 @@ pkl_5 = open("src/est_LGBM.pkl","rb")
 estimator2=pd.read_pickle(pkl_5)
 
 
-# 4. Routes
+# Préparation du dataframe svv (shap_value.values) 'global' qui sera appelé par client id dans la fonction getshap_value(cid:int):
+
+filtered_data=data.drop(['AMT_GOODS_PRICE'], axis =1)
+#print(f"nb de NA sur EXT_SRC_1 avant imputation= {filtered_data['EXT_SOURCE_1'].isna().sum()}")
+transf_data = pd.DataFrame(sampler.transform(filtered_data),columns = filtered_data.columns)
+#print(f"nb de NA sur EXT_SRC_1 après imputation= {transf_data['EXT_SOURCE_1'].isna().sum()}")
+#print(f'transf_data shape = {transf_data.shape}') 
+data_for_shap=transf_data.drop(['SK_ID_CURR'],axis=1)
+data_for_shap_tr=pd.DataFrame(transformer2.transform(data_for_shap),columns=data_for_shap.columns)
+explainer=shap.Explainer(estimator2,data_for_shap_tr)
+shap_values=explainer(data_for_shap_tr,check_additivity=False)
+svv=pd.DataFrame(shap_values.values, columns = data_for_shap.columns).round(2)
+svv['SK_ID_CURR']=transf_data['SK_ID_CURR']  
+#print(f'svv shape = {svv.shape}')
+
+
+## 4. Routes
+
 # 4.1 Index route, opens automatically on http://127.0.0.1:8000
 @api.get('/')
 def index():
@@ -45,8 +84,8 @@ def index():
 def get_ids() -> dict:
     ids=data['SK_ID_CURR'].to_dict()
     return ids
-
-#4.2 Client detail calling an Id
+    
+#4.3 Client detail calling an Id
 @api.get("/client_details/{cid}")
 def get_client_detail(cid:int):
     # NaN in EXT_SOURCE 1,2 and 3 are replaced by 'NA'
@@ -54,18 +93,22 @@ def get_client_detail(cid:int):
     filtered_data=no_nan_data.loc[no_nan_data['SK_ID_CURR']==cid,:].T   
     return filtered_data.iloc[:,0] 
     
-# 4.3 Display describe() to get an overview of the dataset (to compare a single Id to the whole dataset)
+    
+# 4.4 Display describe() to get an overview of the dataset (to compare a single Id to the whole dataset)
 @api.get('/data/')
 def show_data():
     return data.describe().round(2)
 
 
-# 4.4 Has the credit been accepted or refused (depending of the optimal threshold)
+# 4.5 Has the credit been accepted or refused (depending of the optimal threshold)
 @api.get("/prediction/{cid}")
 def get_predictions(cid:int):
     filtered_data=data.loc[data['SK_ID_CURR']==cid,:]
-    # Comme pour l'entrainement du modèle dans le notebook ML, je n'utilise ni SK_ID_CURR ni AMNT GOOD PRICE
+    #print(f'filtered_data shape avant drop = {filtered_data.shape}') #=> 25 ok
+    
+    # Comme pour l'entrainement du modèle dans le notebook ML, je n'utilise ni SK_ID_CURR ni AMNT GOOD PRICE:
     filtered_data=filtered_data.drop(['SK_ID_CURR','AMT_GOODS_PRICE'], axis =1)
+    #print(f'filtered_data après drop shape = {filtered_data.shape}') #=> 23
     prediction = classifier.predict_proba(filtered_data).tolist()
     
     # Seuil determiné dans notebook ML = 0.7
@@ -77,28 +120,28 @@ def get_predictions(cid:int):
     return {'prediction': avis,
             'proba_rembour':round(prediction[0][0],2)}
     
-    # On pourrait ajouter qlq infos sur le crédit (montant du crédit demandé, du bien financé) ainsi que le gain ou la perte estimée
+    # On pourrait ajouter le gain ou la perte estimée
    
-# 4.5 Get the shap values (features that have the more influence on decision) that will be used in dashboard to display graph      
+# 4.6 Get the shap values (features that have the more influence on decision) that will be used in dashboard to display graph      
 @api.get("/shap_val/{cid}")
 def shap_value(cid:int):
-    filtered_data=data.drop(['AMT_GOODS_PRICE'], axis =1)
-    
-    transf_data = pd.DataFrame(sampler.transform(filtered_data),columns = filtered_data.columns) 
-    data_for_shap=transf_data.drop(['SK_ID_CURR'],axis=1)
-    data_for_shap_tr=pd.DataFrame(transformer2.transform(data_for_shap),columns=data_for_shap.columns)
-    #explainer=shap.LinearExplainer(estimator2,data_for_shap_tr)
-    explainer=shap.Explainer(estimator2,data_for_shap_tr)
-    shap_values=explainer(data_for_shap_tr,check_additivity=False)
-    
-    svv=pd.DataFrame(shap_values.values, columns = data_for_shap.columns).round(2)
-    svv['SK_ID_CURR']=transf_data['SK_ID_CURR']  
-    svv=svv.loc[svv['SK_ID_CURR']==cid]
-    svv=svv.drop(['SK_ID_CURR'],axis=1)
-    svv=svv.T
-    return svv.iloc[:,0]
-        
+    ### Cette version fonctionne mais pose 1 pb:
+    ### 1. fait appel à des fichiers .pkl qui constituent le modèle global:
+    ###        - sampler + imputer:  pour imputation des NaN uniquement (le sampler ne s'applique pas)
+    ###        - transformer (standardScaler)
+    ###        - estimateur ()
+    ###  => requiert à chaque deploiement d'un nouveau modèle de déployer également les elements constitutifs de ce modèle => risque d'oubli
+    ### Notes: - pls essais ont été effectués afin de décomposer l'objet classifier en classifier[-3], classifier[-2] et classifier[-1] 
+    ###             => KO pour la majorité des SK_ID_CURR
+    ###             => IndexError: single positional indexer is out-of-bounds
+    ###        - même si je ne conserve que la partie estimateur ca plante 'IndexError: single positional indexer is out-of-bounds'   
 
+    svv2=svv.loc[svv['SK_ID_CURR']==cid]
+    svv2=svv2.drop(['SK_ID_CURR'],axis=1)
+    svv2=svv2.T
+    return svv2.iloc[:,0]
+     
+# pour tester en local, depuis le terminal:
 # uvicorn api_V2:api --reload  
 
 
